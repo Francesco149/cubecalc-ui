68d778ea25ba44c4d9ca0731a98c1ab31fb668c9
Franc[e]sco <lolisamurai@tfwno.gf>
2022-09-09 14:06:26 +0200

    build: fix fast build flags

--- src/build.sh
+++ src/build.sh
@@ -6,14 +6,14 @@ ts=$(date +%s)
 flags="-sALLOW_MEMORY_GROWTH  -sWASM_BIGINT -lidbfs.js"
 flags="$flags -sEXPORTED_FUNCTIONS=_main,_storageAfterInit"
 #flags="$flags -pthread"
-fastbuild="-O0 -DCUBECALC_DEBUG" # ~1s build time
+buildflags="-O0 -DCUBECALC_DEBUG" # ~1s build time
 units=compilation-units/monolith.c
 cc=emcc
 
 for x in $@; do
   case "$x" in
-    rel*) flags="$flags -O3 -flto" ;; # ~6s build time
-    san*) flags="$flags -O3 -g -fsanitize=address,undefined,leak -DCUBECALC_DEBUG" ;;
+    rel*) buildflags="-O3 -flto" ;; # ~6s build time
+    san*) buildflags="-O3 -g -fsanitize=address,undefined,leak -DCUBECALC_DEBUG" ;;
 
     # optionally build as separate compilation units to test that
     # they are not referencing each other in unintended ways.
@@ -23,11 +23,10 @@ for x in $@; do
       units=compilation-units/*_impl.c
       units="$units main.c"
       ;;
-
-    *) flags="$flags $fastbuild" ;;
   esac
 done
 
+flags="$flags $buildflags"
 echo "flags: $flags"
 
 # NOTE: mold does nothing when building for emscripten
