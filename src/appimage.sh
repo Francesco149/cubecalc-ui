f64742103cd46655d77fa448028ca7373863a167
Franc[e]sco <lolisamurai@tfwno.gf>
2022-10-17 12:18:54 +0200

    appimage: consistent output filename

--- src/appimage.sh
+++ src/appimage.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+
 rm -rf builddir
 meson setup builddir --prefix=$(pwd)/AppDir/usr -Dbuildtype=release &&
 cd builddir &&
 meson install &&
-cd .. &&
-curl -OL https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20220822-1/linuxdeploy-x86_64.AppImage &&
-chmod +x ./linuxdeploy-x86_64.AppImage &&
-./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
+cd ..
+
+packagers="
+  https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20220822-1/linuxdeploy-x86_64.AppImage
+"
+
+for x in $packagers; do
+  [ -e "$x" ] || curl -OL $x
+  exename=$(basename "$x")
+  chmod +x ./"$exename"
+  arch=$(echo $exename | sed 's/^linuxdeploy-//; s/\.AppImage$//')
+  ./"$exename" --appdir AppDir --output appimage
+  mv ./CubeCalcUI-*-"${arch}.AppImage" "cubecalc-ui-${arch}.AppImage"
+done
