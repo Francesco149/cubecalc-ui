df9264d66233e086d0e9f4ff067b3d14d7d77589
loli <loli@headpats.uk>
2022-10-23 15:17:37 +0200

    add 32-bit windows build and script to test

---
+++ src/windows-build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# this is just a script specifically for my ubuntu wsl to test the windows build
+arch=${1:-i686}
+
+rm -rf ~/glfw/build
+mkdir ~/glfw/build
+cd ~/glfw/build
+cmake \
+  -DGLFW_BUILD_EXAMPLES=OFF \
+  -DGLFW_BUILD_TESTS=OFF \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DCMAKE_TOOLCHAIN_FILE=~/cubecalc-ui/src/$arch-w64-llvm-mingw32.cmake \
+  -DCMAKE_INSTALL_PREFIX=/opt/llvm-mingw/$arch-w64-mingw32/ \
+  -DCMAKE_BUILD_TYPE=Release \
+  ..
+
+sudo make VERBOSE=1 -j$(nproc) install
+
+cd ~/cubecalc-ui/src
+PKG_CONFIG_PATH=/opt/llvm-mingw/$arch-w64-mingw32/lib/pkgconfig \
+PKG_CONFIG_LIBDIR=/opt/llvm-mingw/$arch-w64-mingw32/lib/pkgconfig \
+meson setup builddir --prefix ~/cubecalc-mingw -Dbuildtype=release --cross-file $arch-w64-llvm-mingw32.txt
+cd builddir
+# workaround for meson bug that is fixed in 0.62.2
+sed -i 's/-Wl,--allow-shlib-undefined//g' build.ninja
+meson install
+cd ..
+rm -rf builddir
+~/cubecalc-mingw/bin/cubecalc-ui.exe
